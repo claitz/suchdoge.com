83e7c8d88051cbe0b4f06b227bcc40394476098a
clait <stefanomus@fastmail.com>
2017-06-18 19:32:12 +0200

    Added text messages. Code written on train's never good.

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,6 @@
  * Created by stefanomuschera on 16/06/2017.
  */
 
-
 // Let there be cookies
 document.getElementById('suchclose').addEventListener('click', function(e) {
     e.preventDefault();
@@ -11,9 +10,17 @@ document.getElementById('suchclose').addEventListener('click', function(e) {
 
 // Let's show some nice text
 setTimeout(function(){
-    document.getElementById('veryfur').classList.remove('hide');
-}, 2000);
+    document.getElementById('muchblep').classList.remove('hide');
+}, 1700);
 
 setTimeout(function(){
-    document.getElementById('muchblep').classList.remove('hide');
-}, 2000);
+    document.getElementById('veryfur').classList.remove('hide');
+}, 2500);
+
+setTimeout(function(){
+    document.getElementById('sopattern').classList.remove('hide');
+}, 3400);
+
+setTimeout(function(){
+    document.getElementById('verydeveloper').classList.remove('hide');
+}, 4000);
