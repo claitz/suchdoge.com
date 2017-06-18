3fdda5763745f16fa4b4f73a1e70196c1483d4f9
clait <stefanomus@fastmail.com>
2017-06-18 19:15:56 +0200

    Code refactoring + text overlay

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,18 @@
  * Created by stefanomuschera on 16/06/2017.
  */
 
+
+// Let there be cookies
 document.getElementById('suchclose').addEventListener('click', function(e) {
     e.preventDefault();
     this.parentNode.style.display = 'none';
 }, false);
+
+// Let's show some nice text
+setTimeout(function(){
+    document.getElementById('veryfur').classList.remove('hide');
+}, 2000);
+
+setTimeout(function(){
+    document.getElementById('muchblep').classList.remove('hide');
+}, 2000);
