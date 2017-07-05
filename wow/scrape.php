b25651318ae4a8e4f3186ab0ff6012cf7aafe6ff
Stefano M <stefanomus@fastmail.com>
2017-07-05 13:06:47 +0200

    Added JQuery

--- wow/scrape.php
+++ wow/scrape.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
     <script>
         $.post(
             'https://graph.facebook.com',
